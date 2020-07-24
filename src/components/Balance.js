36d6d82b1baf299ed77a5442c29f06572121972e
Sam Marston <sammarston15@gmail.com>
2020-07-23 23:34:21 -0600

    got plus/minus & colors working on TransactionList

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const Balance = () => {
+  const {transactions} = useContext(GlobalContext);
+
   return (
     <div>
       <h4>Your Balance</h4>
