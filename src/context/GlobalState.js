4d481a9db68f689aca78b9a0685c1cc5e6f87680
Sam Marston <sammarston15@gmail.com>
2020-07-23 23:19:21 -0600

    finished setting up global state w/ react context

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,5 @@
 import React, {createContext, useReducer} from 'react';
+import AppReducer from './AppReducer'
 
 //initial state
 const initialState = {
@@ -8,4 +9,20 @@ const initialState = {
       { id: 3, text: 'Book', amount: -10 },
       { id: 4, text: 'Camera', amount: 150 }
     ]
+}
+
+
+// create context
+export const GlobalContext = createContext(initialState);
+
+
+// provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (<GlobalContext.Provider value={{
+    transactions: state.transactions
+  }}>
+    {children}
+  </GlobalContext.Provider>)
 }
